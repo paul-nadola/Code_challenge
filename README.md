f7a841a3b3e9f6bbe0a6af6c9f5fafa45e6c0216
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-17 20:27:25 +0300

    Completed Code-Challenge

--- README.md
+++ README.md
@@ -15,4 +15,36 @@ assign your output to the finalOutput variable
 
 
 # YOUR README DESCRIPTION STARTS HERE
+The following programs were written by;
+HTML for structure.
+CSS for styling.
+JavaScript for functionality.
+
+Student Grade Generator.
+This is a program showing the grade achieved with respect to the score a student
+scored on a test.
+Scores range from 0 - 100 with respect to the below scale;
+A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
+
+Speed Detector.
+This is a program that hat takes as input the speed of a car in km/hr e.g 80. If the speed
+is less than 70, it should print “Ok”. Otherwise, for every 5 km/hr above the speed
+limit (70), it should give the driver one demerit point and print the total number
+of demerit points.
+The program suspends the driver's license if the points surpass 12.
+
+Net Salary Calculator.
+This program calcuates an iindividuals net salary based on the tax levied as per the income bracket the individual is in;
+
+Monthly Taxable Pay (Ksh)	Rate of Tax (%)
+Up to 24,000		            10
+24,001 - 50000		            25
+Above 50000	                   	30
+
+Also deductions like insurance and pension contributions are factored in.
+
+
+
+Author; Paul Nadola Oyinga
+copyright @ 2023 | All rights reserved.
 
--- script.js
+++ script.js
@@ -9,17 +9,32 @@ let inputGrade=parseInt(document.getElementById('input-grade').value)
 console.log(inputGrade)
 let finalOutput
 // write Code For the Code challenge
+function gradeCalculator(inputGrade){
+    //Declaring a function calculating different grades
+    if (inputGrade > 79 && inputGrade <= 100){
+     finalOutput = 'A';
+    }// using if statements to create differen ranges with different grades
 
-
-
-
-
-
-
-
-
-
-finalOutput='assign your final output to this variable'
+    else if(inputGrade >= 60 && inputGrade <= 79){
+        finalOutput = 'B';
+    }
+    else if(inputGrade >= 50 && inputGrade <= 59){
+        finalOutput = 'C';
+    }
+    else if(inputGrade >= 40 && inputGrade <= 49){
+        finalOutput = 'D'
+    }
+    else if(inputGrade >= 0 && inputGrade <40 ){
+        finalOutput = 'E'
+    }
+    else{
+        //setting the range of accepteable values to be grade
+        finalOutput = 'Entry not within the test score range';
+    }
+return finalOutput;
+}
+//Assigning the gradeCalculator function to our finalOutput
+finalOutput= gradeCalculator(inputGrade);
 document.getElementById('marks-id').append(finalOutput)
 
 })
@@ -29,41 +44,83 @@ speed.addEventListener('click', (e)=>{
     console.log(inputSpeed)
     let finalOutput
 // write Code For the Code challenge
+function pointsDemeritor(inputSpeed){
+   //Declaring a function that deducts speed as per different speed levels and ultimately revoking the license
+   let speedLimit = 70;
+   let kmperpoint = 5;
+   let points = (inputSpeed - speedLimit)/kmperpoint;
 
+    if(points <= 0){
+        finalOutput = 'Ok';
+    }
+    else if (points > 12){
+        finalOutput = 'LICENSE SUSPENDED'
+    }
+    else{
+        finalOutput = points;
+    }
+    return finalOutput;
+    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-finalOutput='assign your final output to this variable'
+//Assigning our function to our final outout
+finalOutput= pointsDemeritor(inputSpeed);
     document.getElementById('speed-id').append(finalOutput)
 
 })
+
+
 salary.addEventListener('click', (e)=>{
         e.preventDefault()
         let inputSalary=parseInt(document.getElementById('input-salary').value)
         console.log(inputSalary)
-let finalOutput
+let finalOutput;
 // write Code For the Code challenge
+function calculateNetSalary(inputSalary){
+    //variables
+    const basicSalary = inputSalary*0.4//Just incase you need to view the basicSalary
+    const benefits = inputSalary*0.6;//Just incase you need to view total benefits
+    const grossSalary = basicSalary + benefits;//Basically gross salary = inputSalary
+    let taxRate, taxAmount; //calculating taxRates in different income brackets
+    if (grossSalary <= 24000){
+        taxRate = 0.1;
+    }
+    else if (grossSalary <= 50000 && grossSalary >= 24000){
+        taxRate = 0.2;
+    }
+    else {
+        taxRate = 0.3;
+    }
+    taxAmount = grossSalary*taxRate;
+
+    let NHIF; //insurance deduction
+    if (grossSalary <= 24000){
+        NHIF = 500;
+    }
+    else if (grossSalary <= 50000 && grossSalary >= 24000){
+        NHIF = 1000;
+    }
+    else {
+        NHIF = 1500;
+    }
+    let NSSF;//pension deduction
+    if (grossSalary <= 24000){
+        NSSF = 200;
+    }
+    else if(grossSalary <= 50000 && grossSalary >= 24000){
+        NSSF = 500;
+    }
+    else{
+        NSSF = 1000;
+    }
+    const deductions = taxAmount + NHIF + NSSF;//total deductions
+    const netSalary = grossSalary - deductions;
+return netSalary;
+}
 
 
 
-
-
-
-
-
-
-
-
-finalOutput ='assign your final output to this variable'
+finalOutput = calculateNetSalary(inputSalary)
 document.getElementById('salary-id').append(finalOutput)
         
 })
